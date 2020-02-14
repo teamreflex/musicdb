a3d83ed79271a1419e945d66cf58fc4e1326a776
Kyle Ward <kairu@team-reflex.com>
2020-02-15 04:39:13 +1300

    add landing

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
-        <hero></hero>
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-primary shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <h1 class="display-3 text-white">Home</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 <script>
-    import Hero from "./components/Hero";
-
     export default {
         name: "home",
-        components: {
-            Hero,
-        }
     };
 </script>
--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -1,18 +1,112 @@
 <template>
     <div>
-        <hero></hero>
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">Index
+                                    <span>subtext</span>
+                                </h1>
+                                <p class="lead text-white">some kind of description</p>
+                                <div class="btn-wrapper">
+                                    <router-link :to="{ name: 'artists' }"
+                                                 class="btn btn-info mb-3 mb-sm-0">
+                                        <i class="fa fa-music" /> View Artists
+                                    </router-link>
+                                    <router-link :to="{ name: 'register' }"
+                                                 class="btn btn-neutral mb-3 mb-sm-0">
+                                        <i class="fa fa-sign-in" /> Sign up
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+        </div>
+        <section class="section section-lg pt-lg-0 mt--200">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="row row-grid">
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-primary text-uppercase">Download Argon</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
+                                        that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="primary" rounded>design</badge>
+                                        <badge type="primary" rounded>system</badge>
+                                        <badge type="primary" rounded>creative</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="primary" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-success text-uppercase">Build Something</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="success" rounded>business</badge>
+                                        <badge type="success" rounded>vision</badge>
+                                        <badge type="success" rounded>success</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="success" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-planet" type="warning" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-warning text-uppercase">Prepare Launch</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="warning" rounded>marketing</badge>
+                                        <badge type="warning" rounded>product</badge>
+                                        <badge type="warning" rounded>launch</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="warning" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 <script>
-    import Hero from "./components/Hero";
     import {mapGetters} from "vuex";
 
     export default {
         name: "index",
-
-        components: {
-            Hero,
-        },
 
         computed: {
             ...mapGetters({
@@ -22,7 +116,7 @@
 
         mounted() {
             if (this.user) {
-                this.$router.push({ name: 'home' });
+               this.$router.push({ name: 'home' });
             }
         },
     };
