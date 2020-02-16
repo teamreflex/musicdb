001d0ecc546871fb0ebee7a3e4496fbfccad0243
Kyle Ward <kairu@team-reflex.com>
2020-02-16 22:43:09 +1300

    add description to artist card

--- resources/js/views/components/ArtistCard.vue
+++ resources/js/views/components/ArtistCard.vue
@@ -8,7 +8,7 @@
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">{{ artist.name_en }} <span v-if="artist.name_kr">({{ artist.name_kr }})</span></h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">{{ artist.description }}</p>
                 </div>
 
                 <div class="card-footer">
