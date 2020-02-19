468f91aff654d81a7f5094d0b5ecb6d2a304f8ec
Kyle Ward <kairu@team-reflex.com>
2020-02-20 00:27:33 +1300

    fix signed/promo bools being ignored in add

--- app/Services/Collection/CollectionService.php
+++ app/Services/Collection/CollectionService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services\Collection;
 
+use App\Models\Album;
 use App\Models\OwnedItem;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class CollectionService implements CollectionServiceContract
 {
@@ -12,7 +14,16 @@ class CollectionService implements CollectionServiceContract
      */
     public function add(User $user, string $type, int $id, array $data = []): OwnedItem
     {
-        $owned = OwnedItem::where([
+        $owned = OwnedItem::whereHasMorph(
+            'collectable',
+            [Album::class],
+            function (Builder $query) use ($data) {
+                $query->where([
+                    ['is_signed', '=', $data['is_signed']],
+                    ['is_promo', '=', $data['is_promo']],
+                ]);
+            }
+        )->where([
             ['user_id', '=', $user->id],
             ['collectable_type', '=', $type],
             ['collectable_id', '=', $id],
